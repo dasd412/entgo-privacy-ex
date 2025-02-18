b1dd94ba2cdf3d743250a4dc44dd2b8d7f18aaae
yang-yeongjun <dasd412@naver.com>
2025-02-18 11:36:46 +0900

    rule 생성

--- pkg/auth/rule.go
+++ pkg/auth/rule.go
@@ -8,12 +8,35 @@ import (
 
 func DenyIfNoAuthority() privacy.QueryMutationRule {
 	return privacy.ContextQueryMutationRule(func(ctx context.Context) error {
-		return nil
+		role := UserAuthorityFromContext(ctx)
+
+		if role == nil {
+			return privacy.Denyf("no role found in context")
+		}
+		return privacy.Skip
+	})
+}
+
+func DenyIfGuest() privacy.QueryMutationRule {
+	return privacy.ContextQueryMutationRule(func(ctx context.Context) error {
+		role := UserAuthorityFromContext(ctx)
+
+		if role.IsGuest() {
+			return privacy.Deny
+		}
+
+		return privacy.Skip
 	})
 }
 
 func AllowIfAdminOrAuthor() privacy.MutationRule {
 	return privacy.MutationRuleFunc(func(ctx context.Context, mutation ent.Mutation) error {
-		return nil
+		role := UserAuthorityFromContext(ctx)
+
+		if role.IsAdmin() || role.IsAuthor() {
+			return privacy.Allow
+		}
+
+		return privacy.Deny
 	})
 }
